9948ecaf1a1c3b4207c79271018f5d6c9760198e
Tamizharasan <tamizharasan9150@gmail.com>
2025-10-27 22:33:38 +0530

    Add files via upload

---
+++ Single port RAM using EDA PLAYGROUND/Output.v
@@ -0,0 +1,130 @@
+# KERNEL: time=0,mode=x,addr= 0,data=  0,out=  x
+# KERNEL: time=10,mode=1,addr= 0,data=  0,out=  x
+# KERNEL: time=20,mode=1,addr= 1,data=  1,out=  x
+# KERNEL: time=30,mode=1,addr= 2,data=  2,out=  x
+# KERNEL: time=40,mode=1,addr= 3,data=  3,out=  x
+# KERNEL: time=50,mode=1,addr= 4,data=  4,out=  x
+# KERNEL: time=60,mode=1,addr= 5,data=  5,out=  x
+# KERNEL: time=70,mode=1,addr= 6,data=  6,out=  x
+# KERNEL: time=80,mode=1,addr= 7,data=  7,out=  x
+# KERNEL: time=90,mode=1,addr= 8,data=  8,out=  x
+# KERNEL: time=100,mode=1,addr= 9,data=  9,out=  x
+# KERNEL: time=110,mode=1,addr=10,data= 10,out=  x
+# KERNEL: time=120,mode=1,addr=11,data= 11,out=  x
+# KERNEL: time=130,mode=1,addr=12,data= 12,out=  x
+# KERNEL: time=140,mode=1,addr=13,data= 13,out=  x
+# KERNEL: time=150,mode=1,addr=14,data= 14,out=  x
+# KERNEL: time=160,mode=1,addr=15,data= 15,out=  x
+# KERNEL: time=170,mode=1,addr=16,data= 16,out=  x
+# KERNEL: time=180,mode=1,addr=17,data= 17,out=  x
+# KERNEL: time=190,mode=1,addr=18,data= 18,out=  x
+# KERNEL: time=200,mode=1,addr=19,data= 19,out=  x
+# KERNEL: time=210,mode=1,addr=20,data= 20,out=  x
+# KERNEL: time=220,mode=1,addr=21,data= 21,out=  x
+# KERNEL: time=230,mode=1,addr=22,data= 22,out=  x
+# KERNEL: time=240,mode=1,addr=23,data= 23,out=  x
+# KERNEL: time=250,mode=1,addr=24,data= 24,out=  x
+# KERNEL: time=260,mode=1,addr=25,data= 25,out=  x
+# KERNEL: time=270,mode=1,addr=26,data= 26,out=  x
+# KERNEL: time=280,mode=1,addr=27,data= 27,out=  x
+# KERNEL: time=290,mode=1,addr=28,data= 28,out=  x
+# KERNEL: time=300,mode=1,addr=29,data= 29,out=  x
+# KERNEL: time=310,mode=1,addr=30,data= 30,out=  x
+# KERNEL: time=320,mode=1,addr=31,data= 31,out=  x
+# KERNEL: time=330,mode=1,addr=32,data= 32,out=  x
+# KERNEL: time=340,mode=1,addr=33,data= 33,out=  x
+# KERNEL: time=350,mode=1,addr=34,data= 34,out=  x
+# KERNEL: time=360,mode=1,addr=35,data= 35,out=  x
+# KERNEL: time=370,mode=1,addr=36,data= 36,out=  x
+# KERNEL: time=380,mode=1,addr=37,data= 37,out=  x
+# KERNEL: time=390,mode=1,addr=38,data= 38,out=  x
+# KERNEL: time=400,mode=1,addr=39,data= 39,out=  x
+# KERNEL: time=410,mode=1,addr=40,data= 40,out=  x
+# KERNEL: time=420,mode=1,addr=41,data= 41,out=  x
+# KERNEL: time=430,mode=1,addr=42,data= 42,out=  x
+# KERNEL: time=440,mode=1,addr=43,data= 43,out=  x
+# KERNEL: time=450,mode=1,addr=44,data= 44,out=  x
+# KERNEL: time=460,mode=1,addr=45,data= 45,out=  x
+# KERNEL: time=470,mode=1,addr=46,data= 46,out=  x
+# KERNEL: time=480,mode=1,addr=47,data= 47,out=  x
+# KERNEL: time=490,mode=1,addr=48,data= 48,out=  x
+# KERNEL: time=500,mode=1,addr=49,data= 49,out=  x
+# KERNEL: time=510,mode=1,addr=50,data= 50,out=  x
+# KERNEL: time=520,mode=1,addr=51,data= 51,out=  x
+# KERNEL: time=530,mode=1,addr=52,data= 52,out=  x
+# KERNEL: time=540,mode=1,addr=53,data= 53,out=  x
+# KERNEL: time=550,mode=1,addr=54,data= 54,out=  x
+# KERNEL: time=560,mode=1,addr=55,data= 55,out=  x
+# KERNEL: time=570,mode=1,addr=56,data= 56,out=  x
+# KERNEL: time=580,mode=1,addr=57,data= 57,out=  x
+# KERNEL: time=590,mode=1,addr=58,data= 58,out=  x
+# KERNEL: time=600,mode=1,addr=59,data= 59,out=  x
+# KERNEL: time=610,mode=1,addr=60,data= 60,out=  x
+# KERNEL: time=620,mode=1,addr=61,data= 61,out=  x
+# KERNEL: time=630,mode=1,addr=62,data= 62,out=  x
+# KERNEL: time=640,mode=1,addr=63,data= 63,out=  x
+# KERNEL: time=660,mode=0,addr= 0,data= 63,out=  0
+# KERNEL: time=670,mode=0,addr= 1,data= 63,out=  1
+# KERNEL: time=680,mode=0,addr= 2,data= 63,out=  2
+# KERNEL: time=690,mode=0,addr= 3,data= 63,out=  3
+# KERNEL: time=700,mode=0,addr= 4,data= 63,out=  4
+# KERNEL: time=710,mode=0,addr= 5,data= 63,out=  5
+# KERNEL: time=720,mode=0,addr= 6,data= 63,out=  6
+# KERNEL: time=730,mode=0,addr= 7,data= 63,out=  7
+# KERNEL: time=740,mode=0,addr= 8,data= 63,out=  8
+# KERNEL: time=750,mode=0,addr= 9,data= 63,out=  9
+# KERNEL: time=760,mode=0,addr=10,data= 63,out= 10
+# KERNEL: time=770,mode=0,addr=11,data= 63,out= 11
+# KERNEL: time=780,mode=0,addr=12,data= 63,out= 12
+# KERNEL: time=790,mode=0,addr=13,data= 63,out= 13
+# KERNEL: time=800,mode=0,addr=14,data= 63,out= 14
+# KERNEL: time=810,mode=0,addr=15,data= 63,out= 15
+# KERNEL: time=820,mode=0,addr=16,data= 63,out= 16
+# KERNEL: time=830,mode=0,addr=17,data= 63,out= 17
+# KERNEL: time=840,mode=0,addr=18,data= 63,out= 18
+# KERNEL: time=850,mode=0,addr=19,data= 63,out= 19
+# KERNEL: time=860,mode=0,addr=20,data= 63,out= 20
+# KERNEL: time=870,mode=0,addr=21,data= 63,out= 21
+# KERNEL: time=880,mode=0,addr=22,data= 63,out= 22
+# KERNEL: time=890,mode=0,addr=23,data= 63,out= 23
+# KERNEL: time=900,mode=0,addr=24,data= 63,out= 24
+# KERNEL: time=910,mode=0,addr=25,data= 63,out= 25
+# KERNEL: time=920,mode=0,addr=26,data= 63,out= 26
+# KERNEL: time=930,mode=0,addr=27,data= 63,out= 27
+# KERNEL: time=940,mode=0,addr=28,data= 63,out= 28
+# KERNEL: time=950,mode=0,addr=29,data= 63,out= 29
+# KERNEL: time=960,mode=0,addr=30,data= 63,out= 30
+# KERNEL: time=970,mode=0,addr=31,data= 63,out= 31
+# KERNEL: time=980,mode=0,addr=32,data= 63,out= 32
+# KERNEL: time=990,mode=0,addr=33,data= 63,out= 33
+# KERNEL: time=1000,mode=0,addr=34,data= 63,out= 34
+# KERNEL: time=1010,mode=0,addr=35,data= 63,out= 35
+# KERNEL: time=1020,mode=0,addr=36,data= 63,out= 36
+# KERNEL: time=1030,mode=0,addr=37,data= 63,out= 37
+# KERNEL: time=1040,mode=0,addr=38,data= 63,out= 38
+# KERNEL: time=1050,mode=0,addr=39,data= 63,out= 39
+# KERNEL: time=1060,mode=0,addr=40,data= 63,out= 40
+# KERNEL: time=1070,mode=0,addr=41,data= 63,out= 41
+# KERNEL: time=1080,mode=0,addr=42,data= 63,out= 42
+# KERNEL: time=1090,mode=0,addr=43,data= 63,out= 43
+# KERNEL: time=1100,mode=0,addr=44,data= 63,out= 44
+# KERNEL: time=1110,mode=0,addr=45,data= 63,out= 45
+# KERNEL: time=1120,mode=0,addr=46,data= 63,out= 46
+# KERNEL: time=1130,mode=0,addr=47,data= 63,out= 47
+# KERNEL: time=1140,mode=0,addr=48,data= 63,out= 48
+# KERNEL: time=1150,mode=0,addr=49,data= 63,out= 49
+# KERNEL: time=1160,mode=0,addr=50,data= 63,out= 50
+# KERNEL: time=1170,mode=0,addr=51,data= 63,out= 51
+# KERNEL: time=1180,mode=0,addr=52,data= 63,out= 52
+# KERNEL: time=1190,mode=0,addr=53,data= 63,out= 53
+# KERNEL: time=1200,mode=0,addr=54,data= 63,out= 54
+# KERNEL: time=1210,mode=0,addr=55,data= 63,out= 55
+# KERNEL: time=1220,mode=0,addr=56,data= 63,out= 56
+# KERNEL: time=1230,mode=0,addr=57,data= 63,out= 57
+# KERNEL: time=1240,mode=0,addr=58,data= 63,out= 58
+# KERNEL: time=1250,mode=0,addr=59,data= 63,out= 59
+# KERNEL: time=1260,mode=0,addr=60,data= 63,out= 60
+# KERNEL: time=1270,mode=0,addr=61,data= 63,out= 61
+# KERNEL: time=1280,mode=0,addr=62,data= 63,out= 62
+# KERNEL: time=1290,mode=0,addr=63,data= 63,out= 63
+# RUNTIME: Info: RUNTIME_0068 testbench.sv (41): $finish called.
---
+++ Single port RAM using EDA PLAYGROUND/TB.v
@@ -0,0 +1,47 @@
+module tb;
+  parameter data_width =8;
+  parameter addr_width =6;
+  localparam depth = 2**addr_width;
+  reg clk,rst;
+  reg [data_width-1:0]data;
+  reg [addr_width-1:0]addr;
+  reg mode;
+  wire [data_width-1:0] out;
+  integer i;
+  
+  single_port_ram dut(clk,rst,data,addr,mode,out);
+  
+  always #5 clk=~clk;
+  
+  initial begin
+    clk=1;
+    rst=1;
+    mode=1'bx;
+    data=0;
+    addr=0;
+    
+    #10
+    rst=0;
+    
+    mode=1;
+    for(i=0;i<depth;i=i+1)begin
+    addr=i;
+      data=i;
+      #10;
+    end
+    
+    #10;
+    mode=0;
+    for(i=0;i<depth;i=i+1)begin
+      addr=i;
+      #10;
+    end
+    #20;$finish;
+  end
+  
+  initial begin
+    $monitor("time=%0t,mode=%b,addr=%d,data=%d,out=%d",$time,mode,addr,data,out);
+    $dumpfile("dump.vcd");
+    $dumpvars(0);
+  end
+endmodule
